e5a6858ea14eac75b455fefe3b4c7e1975365fd6
Takuji Shimokawa <takuji.shimokawa@gmail.com>
2014-03-17 00:01:22 +0900

    add set command

--- memcached.go
+++ memcached.go
@@ -1,11 +1,38 @@
 package main
 
 import (
+	"github.com/takuji/memcached/protocol"
+	"log"
 	"net"
+	"strings"
 )
 
+type Command struct {
+	key string
+}
+
+func newCommand(s string) (*Command, error) {
+	println(s)
+	elms := strings.Split(s, " ")
+	command := new(Command)
+	command.key = elms[0]
+	return command, nil
+}
+
+func (c *Command) handle(s string) {
+	println(s)
+}
+
 func handleConnection(conn net.Conn) {
-	println("Yahoo!")
+	r := protocol.NewRequestReader(conn)
+
+	for {
+		req, err := r.Read()
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("%+v", req)
+	}
 }
 
 func main() {
---
+++ protocol/transport.go
@@ -0,0 +1,119 @@
+package protocol
+
+import (
+	"bytes"
+	"errors"
+	// "io"
+	"log"
+	"net"
+	"regexp"
+	// "strings"
+	"strconv"
+)
+
+var (
+	commandPattern    *regexp.Regexp
+	setCommandPattern *regexp.Regexp
+)
+
+func init() {
+	commandPattern = regexp.MustCompile(`\A\S+`)
+	setCommandPattern = regexp.MustCompile(`\A(\S+) (\S+) (\S+) (\S+) (\S+)( (\S+))?\r\n(.+)\r\n`)
+}
+
+type RequestReader struct {
+	conn   net.Conn
+	buf    []byte
+	buffer bytes.Buffer
+}
+
+type Request interface {
+}
+
+type SetRequest struct {
+	Command string
+	Key     string
+	Flags   int
+	Exptime int
+	Bytes   int
+	Noreply int
+	Data    string
+}
+
+//
+//  Create a request reader.
+//
+func NewRequestReader(conn net.Conn) *RequestReader {
+	reader := &RequestReader{
+		conn: conn,
+		buf:  make([]byte, 1024),
+	}
+	return reader
+}
+
+//
+//  Read next request and returns a new request
+//
+func (r *RequestReader) Read() (Request, error) {
+	for i := 0; i < 3; i++ {
+		n, err := r.conn.Read(r.buf)
+		if err != nil {
+			log.Fatal(err)
+		}
+		r.buffer.WriteString(string(r.buf))
+		for i := 0; i < n; i++ {
+			r.buf[i] = 0
+		}
+		if n == 2 {
+			return r.MakeRequest()
+		}
+	}
+	return nil, errors.New("Failed to parse input")
+}
+
+func (r *RequestReader) MakeRequest() (Request, error) {
+	s := r.buffer.String()
+	r.buffer.Reset()
+	command := commandPattern.FindString(s)
+	if command == "set" {
+		return makeSetRequest(s)
+	} else if command == "get" {
+
+	} else {
+
+	}
+	return nil, nil
+}
+
+func makeSetRequest(s string) (Request, error) {
+	elms := setCommandPattern.FindStringSubmatch(s)
+	if elms == nil {
+		return nil, errors.New("Bad format request")
+	}
+	flags, err := strconv.Atoi(elms[3])
+	if err != nil {
+		return nil, err
+	}
+	exptime, err := strconv.Atoi(elms[4])
+	if err != nil {
+		return nil, err
+	}
+	bytes, err := strconv.Atoi(elms[5])
+	if err != nil {
+		return nil, err
+	}
+	noreply, err := strconv.Atoi(elms[7])
+	if err != nil {
+		return nil, err
+	}
+	req := &SetRequest{
+		Command: "set",
+		Key:     elms[2],
+		Flags:   flags,
+		Exptime: exptime,
+		Bytes:   bytes,
+		Noreply: noreply,
+		Data:    elms[8],
+	}
+	return req, nil
+}
